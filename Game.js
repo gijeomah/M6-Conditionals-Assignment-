1ca61fb7b45a8902c95354fa4559738b6547c8af
Genesis Ijeomah <gijeomah@my.waketech.edu>
2026-02-16 13:40:33 -0500

    Testing

---
+++ Game.js
@@ -0,0 +1,29 @@
+let random_Number = Math.floor(Math.random()*10)+1;
+// Writing the guess a user has
+//userGuess = Number(prompt("Guess a number between 1-10"));
+let i = 0 
+
+while (true){
+    userGuess = Number(prompt("Guess a number between 1-10"))
+    if (userGuess == 999){
+        break;
+    }
+    if (userGuess > random_Number){
+        alert("The number is too high");
+        i++
+    }
+   if (userGuess < random_Number){
+        alert("The number is too low ");
+        i++
+   } 
+    if (userGuess == random_Number){
+        alert("Congratulations, you have guess it " + i + " amount of times");
+        break;
+    }
+}
+
+    //else;
+       // alert('Congratualtions, you have  guessed it in ')
+     //if (userGuess == 999){
+        //break;
+    //}
